c1d754a9a04802784c2c1505dfd3cbc5664d6858
Alexej Sommer <asommer@yandex.ru>
2015-08-05 18:34:30 +0300

    Solution created

---
+++ BGTaskExample/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// Added
+using Windows.ApplicationModel.Background;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BGTaskExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            foreach (var _task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (_task.Value.Name == "My demo task")
+                {
+                    _task.Value.Unregister(true);
+                }
+            }
+
+
+            // asking to register on lock screen
+            BackgroundAccessStatus accessresult = await BackgroundExecutionManager.RequestAccessAsync();
+
+            BGTaskMD.ExampleBackgroundTask b = new BGTaskMD.ExampleBackgroundTask();
+            b.ShowToast(accessresult.ToString());
+
+            if ((accessresult == BackgroundAccessStatus.Denied)||(accessresult == BackgroundAccessStatus.Unspecified))
+            {
+                return;
+            }
+
+            // just to show result of lock screen registration
+            BGTaskMD.RegisterTask r = new BGTaskMD.RegisterTask();
+            r.RegisterIt();
+
+
+            // unregister ServicingComplete task if it was already registered
+            foreach (var _task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (_task.Value.Name == "My demo task updated")
+                {
+                    _task.Value.Unregister(true);
+                }
+            }
+
+
+            // register ServicingComplete task
+            SystemTrigger taskTrigger = new SystemTrigger(SystemTriggerType.ServicingComplete,false);
+            var bgTaskBuilder = new BackgroundTaskBuilder();
+            bgTaskBuilder.Name = "My demo task updated";
+            bgTaskBuilder.TaskEntryPoint = "BGTaskMD.AppUpdateServicingCompleteTask";
+            bgTaskBuilder.SetTrigger(taskTrigger);
+            BackgroundTaskRegistration task = bgTaskBuilder.Register();
+
+        }
+
+
+    }
+}
---
+++ BGTaskMD/ExampleBackgroundTask.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// added
+using Windows.ApplicationModel.Background;
+
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace BGTaskMD
+{
+ 
+    public sealed class ExampleBackgroundTask : IBackgroundTask
+    {
+
+        Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        volatile bool _cancelRequested = false;
+
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            taskInstance.Canceled += new BackgroundTaskCanceledEventHandler(OnCanceled);
+
+            int taskRunCount = 0;
+            try
+            {
+                taskRunCount = (int)localSettings.Values["AppRunCount"];
+
+            }
+            catch { }
+
+            taskRunCount++;
+
+            try
+            {
+                localSettings.Values["AppRunCount"] = taskRunCount;
+            }
+            catch { }
+
+            if (_cancelRequested != true)
+            {
+                ShowToast("It's my Task - " + taskRunCount);
+            }
+
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            // you can use sender.Task.Name to identity the task  
+            _cancelRequested = true;
+        }
+
+        // snippet to show toast notification
+        public void ShowToast(string whattext)
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            stringElements[0].AppendChild(toastXml.CreateTextNode(whattext));
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            toast.Activated += ToastActivated;
+            toast.Dismissed += ToastDismissed;
+            toast.Failed += ToastFailed;
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        private void ToastFailed(ToastNotification sender, ToastFailedEventArgs args) { }
+        private void ToastDismissed(ToastNotification sender, ToastDismissedEventArgs args) { }
+        private void ToastActivated(ToastNotification sender, object args) { }
+
+    }
+
+
+    
+    public sealed class AppUpdateServicingCompleteTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BGTaskMD.RegisterTask r = new BGTaskMD.RegisterTask();
+            r.RegisterIt();
+        }
+    }
+
+
+
+    public sealed class RegisterTask 
+    {
+
+        public void RegisterIt()
+        {
+
+            foreach (var _task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (_task.Value.Name == "My demo task")
+                {
+                    _task.Value.Unregister(true);
+                }
+            }
+
+            TimeTrigger taskTrigger = new TimeTrigger(15, false);
+            var bgTaskBuilder = new BackgroundTaskBuilder();
+            bgTaskBuilder.Name = "My demo task";
+            bgTaskBuilder.TaskEntryPoint = "BGTaskMD.ExampleBackgroundTask";
+            bgTaskBuilder.SetTrigger(taskTrigger);
+            SystemCondition internetCondition = new SystemCondition(SystemConditionType.InternetAvailable);
+            bgTaskBuilder.AddCondition(internetCondition);
+            BackgroundTaskRegistration task = bgTaskBuilder.Register();
+
+            task.Completed += task_Completed;
+
+        }
+
+
+        void task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            if (sender.Name == "My demo task")
+            {
+
+                try
+                {
+                    args.CheckResult();
+
+                    BGTaskMD.ExampleBackgroundTask b = new BGTaskMD.ExampleBackgroundTask();
+                    b.ShowToast(sender.Name+" is completed");          
+                }
+                catch{}
+
+            }
+        }
+
+
+    }
+
+
+
+
+}
